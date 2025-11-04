2b60e7e79f176e29b96ce98fa81fb7a4d883839d
sigurt51 <philipsigemyr@gmail.com>
2025-11-04 17:41:44 +0100

    steg 4
    min kod för steg 4

--- network_scanner.py
+++ network_scanner.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
 """
 Network Scanner Project
-Students: [Your names here]
+Students: [Philip, Edvin,]
 Date: [Date]
 """
 
-import socket
 import sys
+import socket
 
 
-target = "scanme.nmap.org" # webbplatsen vi vill skanna.
+target = input("vilken sida vill du scanna?: ") # webbplatsen vi vill skanna.
+range_start = int(input("Välj intervallets start tal: "))
+range_end = int(input("välj intervallets slut tal: "))
+timeout = int(input("Hur länge tills timeout?(sec): "))
+open_ports = 0
+
+
+with open("result.txt", "a", encoding="utf-8") as f:
+    f.seek(0)
+    f.truncate()
+    f.write(f"Target: {target} \nRange: {range_start} - {range_end}\nTimeout: {timeout}s\nResult:\n--------\n\n")
+
+print(f"\nPortScanner\nMål: {target}\nIntervall: {range_start}-{range_end}\nTimeout: {timeout}s\n")
 
 try:
-    for port in range(80,90):
+    for port in range(range_start,range_end):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Skapar en ny socket, AF_INET och SOCK_STREAM är default
-        socket.setdefaulttimeout(1) # Gör att default timeout sätts till 1 sekund.
-
+        socket.setdefaulttimeout(timeout) # Gör att default timeout sätts till användarens input
         result = s.connect_ex((target,port)) # Ansluter. Får tillbaks 0 ifall anslutning lyckades.
 
-        if result == 0: # Ger resultatet av anslutningen 0?
-            print(f"port {port} is open") # Om anslutningen ger 0, är porten öppen.
-            s.close() # Man bör tydligen stänga efter sig. Egentligen ska man tydligen använda shutdown med, men i praktik gör folk inte det.
-
+        if result == 0:
+            open_ports += 1
+            print(f"Port {port}: ÖPPEN")
+            with open("result.txt", "a", encoding="utf-8") as f:
+                f.write(f"Port {port}: OPEN\n")
+                s.close()
         else:
-            print(f"port {port} is closed") # Annars är den stängd.
-            s.close()
+            print(f"Port {port}: STÄNGD")
+            with open("result.txt", "a", encoding="utf=8") as f:
+                f.write(f"Port {port}: ClOSED\n")
+                s.close()
+                
+    print(f"\nSkanning slutförd.\n{open_ports} öppna portar.\nResultat sparat i result.txt\n\n")
+    with open("result.txt", "a", encoding="utf-8") as f:
+        f.write(f"\nOpen ports: {open_ports}")
+
 
 except socket.error: # Om det skulle det ske en socket.error
     print("error")
